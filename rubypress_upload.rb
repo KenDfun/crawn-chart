8f70cf28e361ed571f70cdefecfba7d474dbbef2
Ken Okabe <ken@design-fun.com>
2015-10-19 06:52:26 +0900

    html post

--- rubypress_upload.rb
+++ rubypress_upload.rb
@@ -7,17 +7,16 @@ wp = Rubypress::Client.new(
     username: 'okbken',
     password: "pagewebakari3",
     path: '/wordpress/xmlrpc.php',
-    port: 80
 )
 
-title = "rubypress23"
+title = "rubypress33"
 
-content = "aaa"
-# content = ""
-# content = File.open("wordpress.html",'r:utf-8'){|f|
-#   f.read
-# }
-#cat = ['テスト','stock'] # カテゴリのtag_IDを指定...[1,20,50]のように
+# content = "aaa"
+content = ""
+content = File.open("wordpress.html",'r:utf-8'){|f|
+  f.read
+}
+# cat = ['テスト','stock'] # カテゴリのtag_IDを指定...[1,20,50]のように
 cat = "stock"
 tag = "ruby" # カテゴリと同じ様に
 
@@ -26,7 +25,7 @@ post_id = wp.newPost(
     blog_id: 1, #通常は1でOK
     content:{
         post_status: 'publish', #公開:publish　下書き:draft
-        post_date: Time.now,
+        post_date: Time.now - 32400,
         post_content: content,
         post_title: title,
         terms: {
@@ -41,5 +40,5 @@ rescue XMLRPC::FaultException => e
   puts e.faultString
 end
 
-#p post_id
-#p wp.get
+p post_id
+# p wp.get
