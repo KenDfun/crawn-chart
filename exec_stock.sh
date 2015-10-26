c1c61413f03dfce480e56106ec43bdf5c7d0e473
kabuchk <kabuchk@still.dreamhost.com>
2015-10-26 13:39:56 -0700

    change path

--- exec_stock.sh
+++ exec_stock.sh
@@ -1,3 +1,4 @@
 #!/bin/sh
 
-ruby /home/kabuchk/src/crawn-chartstock.rb < ticker.text
+ruby /home/kabuchk/src/crawn-chart/stock_18.rb < /home/kabuchk/src/crawn-chart/ticker.txt
+ruby /home/kabuchk/src/crawn-chart/upload_sftp.rb < /home/kabuchk/src/crawn-chart/ticker.txt
